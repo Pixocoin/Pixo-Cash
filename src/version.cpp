be83f2ba2dd5e8ac49441d5002c30696fa155ada
Eman Bayani <emanbayani@yahoo.com>
2018-03-22 01:20:08 -0500

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -46,7 +46,7 @@ const std::string CLIENT_NAME("PIXOC");
     "v" STRINGIFY(maj) "." STRINGIFY(min) "." STRINGIFY(rev) "." STRINGIFY(build) "-g" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj,min,rev,build) \
-    "v" STRINGIFY(maj) "." STRINGIFY(min) "." STRINGIFY(rev) "." STRINGIFY(build) "-unk"
+    "v" STRINGIFY(maj) "." STRINGIFY(min) "." STRINGIFY(rev) "." STRINGIFY(build) "-PIXOC"
 
 #ifndef BUILD_DESC
 #    ifdef GIT_COMMIT_ID
