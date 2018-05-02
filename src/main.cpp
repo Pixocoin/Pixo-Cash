32bc8cc445a35670857342e6e2d8990aec075902
Eman Bayani <emanbayani@yahoo.com>
2018-05-01 22:43:21 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -698,7 +698,7 @@ int CMerkleTx::GetBlocksToMaturity() const
 {
     if (!IsCoinBase())
         return 0;
-    return max(0, (COINBASE_MATURITY+15) - GetDepthInMainChain());
+    return max(0, (COINBASE_MATURITY+20) - GetDepthInMainChain());
 }
 
 
