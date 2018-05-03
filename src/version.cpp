35b38d80ab5d26509830dfb62e5807235b333f49
Eman Bayani <emanbayani@yahoo.com>
2018-05-02 21:33:30 -0500

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -12,7 +12,7 @@
 const std::string CLIENT_NAME("PIXOC");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-DEV"
+#define CLIENT_VERSION_SUFFIX   "-beta"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
