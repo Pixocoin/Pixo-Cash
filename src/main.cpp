a7b90bf468b7e7b083cfbfe48594e5d7712f7e76
Eman Bayani <emanbayani@yahoo.com>
2018-05-02 19:43:53 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -825,7 +825,7 @@ uint256 static GetOrphanRoot(const CBlock* pblock)
         pblock = mapOrphanBlocks[pblock->hashPrevBlock];
     return pblock->GetHash();
 }
-
+// miner's reward
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 3 * COIN;
