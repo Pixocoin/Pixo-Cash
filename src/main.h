fad6df6a31b0184ad9aff59fd6205c2feddd1056
Eman Bayani <emanbayani@yahoo.com>
2018-05-20 09:03:39 -0500

    Update main.h

--- src/main.h
+++ src/main.h
@@ -35,7 +35,7 @@ class CNode;
 
 // for now, we leave the block size at 2 MB, meaning we support roughly 4800 transactions
 // per block, which means about 320 tps
-static const unsigned int MAX_BLOCK_SIZE = 1000000;
+static const unsigned int MAX_BLOCK_SIZE = 2000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
