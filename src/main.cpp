88922d8713b446f7ed4485f6e15e9a1a0fd36c59
Eman Bayani <emanbayani@yahoo.com>
2018-05-21 18:41:34 -0500

    nReTargetHistoryFact = 5

--- src/main.cpp
+++ src/main.cpp
@@ -862,7 +862,7 @@ static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
 // Thanks: Balthazar for suggesting the following fix
 // https://bitcointalk.org/index.php?topic=182430.msg1904506#msg1904506
-static const int64 nReTargetHistoryFact = 1; // look at 3 times the retarget
+static const int64 nReTargetHistoryFact = 5; // look at 5 times the retarget
                                              // interval into the block history
 
 //
