2019125a3870d8c0016746e5d6ce6106338c49e5
Eman Bayani <emanbayani@yahoo.com>
2018-09-29 19:48:12 -0500

    Internet getting jammed!
    Checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -151,6 +151,7 @@ namespace Checkpoints
         (     10676, uint256("0xb13d749d1eea734458f0fc43bab7085eef005fd302370bf20e9cf700d68aa2f3"))
         (     10754, uint256("0xaa0e8469cc505b4b10c66502fcc8ef1b5398ace5faff47dd9e6b72348dcb02ed"))
         (     10844, uint256("0x3ee2c10ee946e48dcb0a9ee8bc1cf8b49a7b13c561446002cd3a5127d5f8f559"))
+        (     10908, uint256("0x1a2392c4a2f679395683269b6699170edddeadc55cbeaa9bd02cd04dc0598f36"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
