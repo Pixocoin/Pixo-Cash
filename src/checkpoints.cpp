a46456617b335da8a0c0782a7967fb89ba83a3b7
Eman Bayani <emanbayani@yahoo.com>
2018-12-24 02:11:34 -0600

    Checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -186,6 +186,7 @@ namespace Checkpoints
         (     16546, uint256("0x778ed997a80e575d0105d21e9b1b4c6c2ea6bf37fc23153a031571eb1f6f718d"))
         (     16646, uint256("0x6b6dead93992ff9075c0c09c5d73dcdb3592b74bbc463e63b66860d136e1b28a"))
         (     16966, uint256("0x854c2f762bca4b02c83608edb74895fbab3b3c10f3176eac9582357bf7c051f7"))
+        (     17213, uint256("0x212954f0bc8da489a39b33468fcc06bcf2c5448e65cee2f465d40f0d3047db41"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
