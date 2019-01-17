c2cf4402c8e29a0249052efc391be78efad29324
Eman Bayani <emanbayani@yahoo.com>
2019-01-16 23:07:02 -0600

    Checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -194,6 +194,7 @@ namespace Checkpoints
         (     18011, uint256("0xb5b2cbffda69f3fb3459151b9558e50063d2f0b5605dfc8b05ba945cb502d02f"))
         (     18097, uint256("0xda2ebcd3ea4b1d1d3b8bccc8ac11a72ccd494c673d8838766925969a84b3f23e"))
         (     18497, uint256("0x3407704ea6bdbe838e996d306d27c4126a0cee387a7be617911923ab59f3c72a"))
+        (     18770, uint256("0x719874f60ed8216fbb774632559e2eed843b3baedc841407eaf4775bbb669236"))
         
         
         
